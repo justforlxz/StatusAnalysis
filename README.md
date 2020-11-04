5756c0abdedeeda0cee08d05f28b9fee5c2a6f7a
lxz <justforlxz@gmail.com>
2020-11-04 17:45:16 +0800

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 使用status-analysis进行数据采集，再通过excel.py进行svg报表生成。
+
+例如以下例子：
+
+使用status-analysis采集数据
+
+```shell
+./status-analysis -c /tmp/test.csv "dde-control-center --show"
+```
+
+使用excel.py对采集的数据进行svg报表生成
+
+```shell
+python3 excel.py --csv /tmp/test.csv --type svg --output /tmp/test.svg
+```
--- excel.py
+++ excel.py
@@ -2,30 +2,31 @@
 # -*- coding: UTF-8 -*-
 
 import sys
-import xlrd
 import pygal
 import string
 import getopt
 import datetime
+import csv
 
 def main(argv):
-    xlsfile = ''
+    csvfile = ''
     pid = []
     outputfile = ''
     type = ''
     try:
         opts, args = getopt.getopt(
-            argv, "-h-xls:-pid-type:-output:", ["xls=", "pid=", "type=", "output="])
+            argv, "-h-csv:-pid-type:-output:", ["csv=", "pid=", "type=", "output="])
     except getopt.GetoptError:
-        print('excel.py -xls <xls file> -pid <pid> -output <outputfile>')
+        print('excel.py -csv <csv file> -pid <pid> -output <outputfile>')
         sys.exit(2)
 
     for opt, arg in opts:
         if opt == '-h':
-            print('excel.py -xls <xls file> -pid <pid> -output <outputfile>')
+            print(
+                'excel.py -csv <csv file> --type <type> -pid <pid> -output <outputfile>')
             sys.exit()
-        elif opt in ("-xls", "--xls"):
-            xlsfile = arg
+        elif opt in ("-csv", "--csv"):
+            csvfile = arg
         elif opt in ("-output", "--output"):
             outputfile = arg
         elif opt in ("-column", "--column"):
@@ -39,57 +40,56 @@ def main(argv):
         print("type only svg or png")
         sys.exit()
 
-    wb = xlrd.open_workbook(xlsfile)
-    table = wb.sheets()[0]
-    line = pygal.Line()
-
     if not pid:
-        for r in range(table.nrows):
-            pid.append(str(int(table.row_values(r)[1])))
-        pid = list(set(pid))
+        with open(csvfile, newline='') as file:
+            spamreader = csv.reader(file, delimiter=",")
+            for row in spamreader:
+                pid.append(str(int(row[1])))
+            pid = list(set(pid))
 
-    for n in pid:
-        tmp_list = []
-        time_list = []
-        name_ = ""
-        lastReadIO = 0
-        lastWriteIO = 0
-        isFind = False
-        for r in range(table.nrows):
-            row = table.row_values(r)
-            time = row[0]
-            pid = str(int(row[1]))
-            name = row[2]
-            cpu_usage = row[3]
-            vmrss = row[4]
-            readio = row[5]
-            writeio = row[6]
+    with open(csvfile, newline='') as file:
+        line = pygal.Line()
+        for n in pid:
+            tmp_list = []
+            time_list = []
+            name_ = ""
+            lastReadIO = 0
+            lastWriteIO = 0
+            isFind = False
+            spamreader = csv.reader(file, delimiter=",")
+            for row in spamreader:
+                time = int(row[0])
+                pid = str(int(row[1]))
+                name = row[2]
+                cpu_usage = float(row[3])
+                vmrss = row[4]
+                readio = row[5]
+                writeio = row[6]
 
-            if cpu_usage < 1:
-                continue
+                if cpu_usage < 1:
+                    continue
 
-            time_list.append(datetime.datetime.fromtimestamp(time))
+                time_list.append(datetime.datetime.fromtimestamp(time))
 
-            if (pid == str(n)):
-                tmp_list.append(cpu_usage)
-                name_ = name
-                lastReadIO = readio
-                lastWriteIO = writeio
-                isFind = True
-            else:
-                tmp_list.append(None)
+                if (pid == str(n)):
+                    tmp_list.append(cpu_usage)
+                    name_ = name
+                    lastReadIO = readio
+                    lastWriteIO = writeio
+                    isFind = True
+                else:
+                    tmp_list.append(None)
 
-        if isFind:
-            line.add(name_, tmp_list)
-            line.x_labels = time_list
+            if isFind:
+                line.add(name_, tmp_list)
+                line.x_labels = time_list
 
-    line.legend_at_bottom = True
+        line.legend_at_bottom = True
+        if type == "svg":
+            line.render_to_file(outputfile)
+        if  type == "png":
+            line.render_to_png(outputfile)
 
-    if type == "svg":
-        line.render_to_file(outputfile)
-
-    if  type == "png":
-        line.render_to_png(outputfile)
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    main(sys.argv[1:])
